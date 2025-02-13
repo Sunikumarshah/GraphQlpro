dae344a3b895bf247d4aa100839933047f7349e0
Sunilshah <shahsunilkumar373@gamil.com>
2025-02-13 12:07:09 +0530

    add env file

--- client/src/ApolloClient.jsx
+++ client/src/ApolloClient.jsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql', 
+  uri: `${import.meta.env.VITE_BACKEND_LINK}/graphql`, 
   cache: new InMemoryCache()
 });
 
